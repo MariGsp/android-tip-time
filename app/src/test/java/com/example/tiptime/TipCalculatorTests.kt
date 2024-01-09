0b62544924a22ef722eb6ddee09d4168d68799ee
Mari <marygt182@gmail.com>
2024-01-09 11:57:42 +0000

    Add instrumentation test

--- app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -6,8 +6,8 @@ import java.text.NumberFormat
 
 class TipCalculatorTests {
     @Test
-    fun calculateTip_20PercentNoRoundup(){
-        val amount =  10.00
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
         val tipPercent = 20.00
         val expectedTip = NumberFormat.getCurrencyInstance().format(2)
         val actualTip = calculateTip(amount, tipPercent, false)
